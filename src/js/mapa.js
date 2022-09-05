2b316559e8f5d2965807179d9c1bb22db235f013
claudiomonzoni <claudiomonzoni@hotmail.com>
2022-09-05 12:29:50 -0500

    casi acabamos, se agrego reservaciones y mapa

---
+++ src/js/mapa.js
@@ -0,0 +1,76 @@
+import L from 'leaflet'
+
+export class Mapa {
+  constructor() {
+    this.mapa = this.inicializarMapa()
+    this.markers = new L.LayerGroup()
+  }
+
+  inicializarMapa() {
+    // Inicializar y obtener la propiedad del mapa
+    const map = L.map('mapa',{
+      scrollWheelZoom:false,
+      zoomOffset: -1
+    }).setView([17.639544, -101.557177], 16)
+    const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>'
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; ' + enlaceMapa + ' Contributors',
+      maxZoom: 20,
+    }).addTo(map)
+
+    return map
+  }
+
+  obtenerDatos() {
+    const pines = {
+      resultados: [
+        {
+          latitude: '17.642541',
+          longitude: '-101.552695',
+          titulo: 'Posada del Palmar Zihuatanejo',
+          calle: 'Paseo del Palmar #66 Col. Centro, Zihuatanejo, Gro, México CP. 40880',
+        },
+      ],
+    }
+
+    this.mostrarPines(pines.resultados)
+  }
+
+  mostrarPines(datos) {
+    // limpio marker para mostrar otra busqueda
+    this.markers.clearLayers()
+
+    datos.forEach((dato) => {
+      //destructurig para obtener las prop del obj
+      const { latitude, longitude, titulo, calle } = dato
+
+      // crear un globo de info
+      const globo = L.popup().setContent(`
+  <h3>${titulo} </h3>
+  <p> Calle: ${calle}</p>
+  `)
+      // objeto de los markers
+      const elpin = L.icon({
+        iconUrl:
+          'https://www.idmtest.com/style/img/ic-marker.svg',
+        //shadowUrl: "leaf-shadow.png",
+
+        iconSize: [50, 69], // size of the icon
+        //shadowSize: [50, 64], // size of the shadow
+        iconAnchor: [35, 45], // point of the icon which will correspond to marker's location
+        //shadowAnchor: [4, 62], // the same for the shadow
+        popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+      })
+
+      //agregar el pin por obj
+      // corchete porque es array
+      const marker = new L.marker(
+        [parseFloat(latitude), parseFloat(longitude)],
+        { icon: elpin }
+      ).bindPopup(globo)
+      //agregamos cada pin al layer del constructor
+      this.markers.addLayer(marker)
+    })
+    this.markers.addTo(this.mapa)
+  }
+}
---
+++ src/js/reservas.js
@@ -0,0 +1,80 @@
+import moment from "moment";
+import "moment/locale/es";
+
+const revisar = document.getElementById("revisar");
+//inputs
+const nombre = document.getElementById("nombre");
+const habitaciones = document.getElementById("habitaciones");
+const llegada = document.getElementById("llegada");
+const salida = document.getElementById("salida");
+const adultos = document.getElementById("adultos");
+const ninos = document.getElementById("ninos");
+const btncotizar = document.getElementById("btncotizar");
+
+
+//user agent
+const ua = navigator.userAgent;
+
+//obtengo los datos de los inputs
+
+//si es cel app si es pc web.app
+const actualizar = (e) => {
+  e.preventDefault();
+
+  if (
+    nombre.value === "" ||
+    habitaciones.value === "" ||
+    llegada.value === "" ||
+    salida.value === "" ||
+    adultos.value === "" ||
+    ninos.value === ""
+  ) {
+    console.log("vacio");
+  } else {
+    btncotizar.classList.remove("desactivado");
+  }
+
+  //comprobar si es cel o pc
+  let whats = "";
+  if (/Mobile/i.test(ua)) {
+    whats = "https://api.whatsapp.com/send/?phone=&text=";
+  } else {
+    whats = "https://web.whatsapp.com/send/?phone=5217551149568&text=";
+  }
+  //obtener las noches
+
+  const llegadaM = moment(llegada.value).format("dddd D MMMM YYYY");
+  const salidaM = moment(salida.value).format("dddd D MMMM YYYY");
+
+  const mensaje = `
+  <hr>
+  <b> Por favor revise los datos para su reservación</b>
+  <hr>
+Name / Nombre: <b> ${nombre.value} </b> <br>
+Tipo de habitación: <b> ${habitaciones.value} </b> <br>
+Fecha de llegada: <b> ${llegadaM} </b> <br>
+Fecha de Salida: <b> ${salidaM} </b> <br>
+Numero de adultos: <b> ${adultos.value}</b> </br>
+Numero de niños: <b> ${ninos.value}</b>
+`;
+
+  revisar.innerHTML = mensaje;
+  envio(whats, llegadaM, salidaM);
+};
+
+// fin actualizar
+
+const envio = (whats, llegadaM, salidaM) => {
+  const url = `
+  ${whats}Hola,%20me%20contacto%20desde%20zihuacentro.com,%20deseo%20cotizar:%0aNombre:%20${nombre.value}%0aHabitación:%20${habitaciones.value},%0aNo.%20de%20adultos:%20${adultos.value},%0aNo.%20de%20niños:%20${ninos.value}%0aFecha%20de%20Llagada:%20${llegadaM}%0aFecha%20de%20Salida:%20${salidaM}
+  %0aComentarios:%20
+  `;
+  btncotizar.href = url;
+};
+
+nombre.addEventListener("focusout", actualizar);
+habitaciones.addEventListener("focusout", actualizar);
+llegada.addEventListener("focusout", actualizar);
+salida.addEventListener("focusout", actualizar);
+adultos.addEventListener("focusout", actualizar);
+ninos.addEventListener("focusout", actualizar);
